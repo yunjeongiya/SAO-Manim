4fa37b6eba9e8d84227cf9120070f530a258be75
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-05-24 11:40:18 +0900

    feat: describeMinimum 구현

--- SAO236모20번/consts.py
+++ SAO236모20번/consts.py
@@ -1,7 +1,6 @@
 from manim import *
-Tex.set_default(font_size=30)
 
-TITLE = Tex("23학년도 6월 평가원 20번")
-TEXT1 = Tex(r"최고차항의 계수가 2인 이차함수 $f(x)$", r"에 대하여")
-TEXT2 = Tex("함수 ", r"$g(x)={\int_x^{x+1}}|f(t)|dt$", r"는 ", r"$x=1$과 $x=4$", "에서 ", "극소",r"이다.")
-TEXT3 = Tex(r"$f(0)$의 값을 구하시오. [4점]")
+TITLE = Tex("23학년도 6월 평가원 20번", font_size=30)
+TEXT1 = Tex(r"최고차항의 계수가 2인 이차함수 $f(x)$", r"에 대하여", font_size=30)
+TEXT2 = Tex("함수 ", r"$g(x)={\int_x^{x+1}}|f(t)|dt$", r"는 ", r"$x=1$과 $x=4$", "에서 ", "극소",r"이다.", font_size=30)
+TEXT3 = Tex(r"$f(0)$의 값을 구하시오. [4점]", font_size=30)
--- SAO236모20번/main.py
+++ SAO236모20번/main.py
@@ -5,6 +5,7 @@ config.max_files_cached = -1
 
 class main(Scene):
     def construct(self):
-        self.next_section()
+        self.next_section(skip_animations=True)
         texts = showProblem(self)
         describeProblem(self, texts)
+        minimumDescription = describeMinimum(self, texts)
