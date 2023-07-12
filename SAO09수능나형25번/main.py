dd02fc76aecff40a30b9e934f8db051aa33e01c1
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-07-12 16:54:38 +0900

    feat: play 오버라이드

--- SAO09수능나형25번/main.py
+++ SAO09수능나형25번/main.py
@@ -3,6 +3,14 @@ from sections import *
 
 config.max_files_cached = -1
 class main(Scene):
+    count = 0
+    def play(self, *args, **kwargs):
+        args = list(args)
+        args.append(Wait(2))
+        super().play(*args, **kwargs)
+        self.next_section(str(self.count))
+        self.count += 1
+
     def construct(self):
         showProblem(self)
         toFadeOut = showExample(self)
