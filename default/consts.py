08f73a847d23f23ce86f3f8ec9e3b05b72a1f7cd
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-07-18 13:33:06 +0900

    feat: default consts에 TEXTS 양식 추가

--- default/consts.py
+++ default/consts.py
@@ -2,3 +2,7 @@ from manim import *
 from library.consts import *
 
 TITLE = Text("", font=GOTHIC)
+
+TEXTS = VGroup(
+    VGroup(TITLE.scale(0.8), Underline(TITLE)),
+).arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.3).to_corner(UL)
