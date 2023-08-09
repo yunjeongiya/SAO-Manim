1dde2f4aa1fd9877e0d1444d968337acfa05b983
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-08-09 16:39:30 +0900

    fix: default consts에 TEX_SCALE 추가

--- default/consts.py
+++ default/consts.py
@@ -7,8 +7,9 @@ VIOLET = "#FF6AFF"
 #fonts
 GOTHIC = "NanumBarunGothic"
 
+TEX_SCALE = 0.8
 TITLE = Text("", font=GOTHIC)
 
 TEXTS = VGroup(
     VGroup(TITLE.scale(0.8), Underline(TITLE)),
-).arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.3).to_corner(UL)
+).arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.3).scale(TEX_SCALE).to_corner(UL)
