9baa46805aff42eee5d80859a4b567506c4a1b92
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-09-07 21:26:52 -0700

    feat: consts에 OPTIONS 추가

--- default/consts.py
+++ default/consts.py
@@ -9,6 +9,10 @@ GOTHIC = "NanumBarunGothic"
 
 TEX_SCALE = 0.8
 TITLE = Text("", font=GOTHIC)
+OPTIONS = VGroup(
+    VGroup(Tex("① $$"), Tex("② $$"), Tex("③ $$")).arrange(buff=2),
+    VGroup(Tex("④ $$"), Tex("⑤ $$")).arrange(buff=2)
+).arrange(DOWN, center=False, aligned_edge=LEFT, buff=0.3)
 
 TEXTS = VGroup(
     VGroup(TITLE.scale(0.8), Underline(TITLE)),
