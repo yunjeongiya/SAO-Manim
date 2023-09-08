b5e01ec5aa3d8cc6a511b8447c1dff6787514ddc
윤정 <70215620+yunjeongiya@users.noreply.github.com>
2023-09-07 21:26:35 -0700

    feat: utils에 searchShapesInTex 추가

--- library/utils.py
+++ library/utils.py
@@ -130,4 +130,16 @@ class BasicGraphDict(VDict):
     
     def getScaleRatio(self):
         return Line(self["ax"].c2p(0, 0), self["ax"].c2p(1, 0)).get_length()
-    
+
+def searchShapesInTex(text:VMobject, shape:VMobject):
+    T = TransformMatchingShapes
+    results = []
+    l = len(shape.submobjects[0])
+    shape_aux = VMobject()
+    shape_aux.points = np.concatenate([p.points for p in shape.submobjects[0]])
+    for i in range(len(text.submobjects[0])):
+        subtext = VMobject()
+        subtext.points = np.concatenate([p.points for p in text.submobjects[0][i:i+l]])
+        if T.get_mobject_key(subtext) == T.get_mobject_key(shape_aux):
+            results.append(slice(i, i+l))
+    return results
